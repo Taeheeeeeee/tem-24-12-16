10a5cd9b901ca61241a5969bc5b896d108541ce0
Taeheeeeeee <taeheemin62@gmail.com>
2024-12-17 10:29:00 +0900

    write

---
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -0,0 +1,39 @@
+package com.ll.tem.domain.post.post.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/posts")
+public class PostController {
+    @GetMapping("/write")
+    @ResponseBody
+    public String showWrite() {
+        return """
+                <form action="/posts/doWrite">
+                    <input type="text" name="title" placeholder="제목">
+                    <textarea name="content" placeholder="내용"></textarea>
+                    <button type="submit">글쓰기</button>
+                </form>
+                """;
+    }
+
+    @GetMapping("/doWrite")
+    @ResponseBody
+    public String write(
+            @RequestParam("title") String title,
+            @RequestParam("content") String content
+    ) {
+        return """
+                <h1>글쓰기 완료</h1>
+                
+                <div>
+                    <h2>%s</h2>
+                    <p>%s</p>
+                </div>
+                """.formatted(title, content);
+    }
+}
