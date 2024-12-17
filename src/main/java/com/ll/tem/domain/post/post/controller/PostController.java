cac8e27fcbe5f349f89a853e384b6eb7d8e5e8dc
Taeheeeeeee <taeheemin62@gmail.com>
2024-12-17 12:17:12 +0900

    유효성 검사

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -1,23 +1,31 @@
 package com.ll.tem.domain.post.post.controller;
 
-import jakarta.validation.constraints.Min;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/posts")
-@Validated
 public class PostController {
-
     private String getFormHtml(String errorMessage, String title, String content) {
         return """
                 <div>%s</div>
                 <form method="POST">
                     <input type="text" name="title" placeholder="제목" value="%s">
+                    <br>
                     <textarea name="content" placeholder="내용">%s</textarea>
+                    <br>
                     <button type="submit">글쓰기</button>
                 </form>
                 """.formatted(errorMessage, title, content);
@@ -29,18 +37,37 @@ public class PostController {
         return getFormHtml("", "", "");
     }
 
+    @AllArgsConstructor
+    @Getter
+    @ToString
+    public static class PostWriteForm {
+        @NotBlank(message = "제목을 입력해주세요.")
+        @Length(min = 5, message = "제목을 5자 이상 입력해주세요.")
+        private String title;
+
+        @NotBlank(message = "내용을 입력해주세요.")
+        @Length(min = 10, message = "내용을 10자 이상 입력해주세요.")
+        private String content;
+    }
+
     @PostMapping("/write")
     @ResponseBody
     public String write(
-            @RequestParam("title")
-            @NotBlank(message = "제목을 입력해주세요")
-            @Length(min = 5, message = "제목을 5자 이상 입력해주세요")
-            String title
-            , @RequestParam("content")
-            @NotBlank(message = "내용을 입력해주세요")
-            @Length(min = 10, message = "내용을 10자 이상 입력해주세요")
-            String content
+            @Valid PostWriteForm form
+            , BindingResult bindingResult
     ) {
+        if(bindingResult.hasErrors()){
+            String errorMessages = bindingResult.getAllErrors()
+                    .stream()
+                    .map(err -> err.getDefaultMessage())
+                    .collect(Collectors.joining("<br>"));
+
+            return getFormHtml(
+                    errorMessages
+                    , form.getTitle()
+                    , form.getContent());
+        }
+
         return """
                 <h1>글쓰기 완료</h1>
                 
@@ -48,6 +75,6 @@ public class PostController {
                     <h2>%s</h2>
                     <p>%s</p>
                 </div>
-                """.formatted(title, content);
+                """.formatted(form.getTitle(), form.getContent());
     }
 }
