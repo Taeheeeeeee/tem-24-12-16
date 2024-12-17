1490601aa612c0d45a17cbc25a5196705fd8d28a
Taeheeeeeee <taeheemin62@gmail.com>
2024-12-17 11:21:08 +0900

    dry원칙 적용

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -6,6 +6,18 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/posts")
 public class PostController {
+
+    private String getFormHtml(String errorMessage, String title, String content) {
+        return """
+                <div>%s</div>
+                <form method="POST">
+                    <input type="text" name="title" placeholder="제목" value="%s">
+                    <textarea name="content" placeholder="내용">%s</textarea>
+                    <button type="submit">글쓰기</button>
+                </form>
+                """.formatted(errorMessage, title, content);
+    }
+
     @GetMapping("/write")
     @ResponseBody
     public String showWrite() {
@@ -24,6 +36,22 @@ public class PostController {
             @RequestParam("title") String title,
             @RequestParam("content") String content
     ) {
+        if (title.length() < 5) {
+            return getFormHtml("제목을 5자 이상 입력해주세요", title, content);
+        }
+
+        if (title == null || title.isBlank()) {
+            return getFormHtml("제목을 입력해주세요", title, content);
+        }
+
+        if (content.length() < 10) {
+            return getFormHtml("내용을 10자 이상 입력해주세요", title, content);
+        }
+
+        if (content == null || content.isBlank()) {
+            return getFormHtml("내용을 입력하세요", title, content);
+        }
+
         return """
                 <h1>글쓰기 완료</h1>
                 
